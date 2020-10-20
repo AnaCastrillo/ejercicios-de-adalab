e1ce7f9e87e45476fbc746b568167be97e562abd
AnaCastrillo <callmeferre@gmail.com>
2020-10-20 20:56:12 +0200

    M2-L13-E1&2

---
+++ module-2/lection-13/ex-1/main.js
@@ -0,0 +1,5 @@
+const marks = [5, 4, 6, 7, 9];
+const markChange = (mark) => mark + 1;
+const inflatedMarks = marks.map(markChange);
+
+console.log(inflatedMarks);
---
+++ module-2/lection-13/ex-2/main.js
@@ -0,0 +1,3 @@
+const names = ['María', 'Lucía', 'Susana', 'Rocío', 'Inmaculada'];
+const greeting = (name) => 'Bienenida, ' + name;
+const nameGreeting = names.map(greeting);
